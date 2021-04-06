b71e26fb257891532ebd0d33a24026c9a2429f79
Shronik04 <shronik.chandna@mail.vinove.com>
2021-04-06 18:22:10 +0530

    errorPageUpdate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Switch,Route } from "react-router-dom"
+import Error from './components/Error'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="*" component={Error} />
+      </Switch>
     </div>
   );
 }
